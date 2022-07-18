5c566ea2e12f6d9f90e8c586636e034bc9d36787
ashishmaurya089 <ashishmaurya089@gmail.com>
2022-07-18 12:09:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-yso8up
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yso8up)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  r ="raj"
+  render() {
+    return <div>hello </div>;
+  }
+}
+
+export default App;
